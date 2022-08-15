19fb56b2064354b9d252f145592fa7ccbae3ccae
sohan-gts <sohan@gtsinfosoft.com>
2022-08-15 19:04:16 +0530

    init-code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'msal-example';
+export class AppComponent implements OnInit {
+
+constructor(private authService: MsalService){
+}
+
+  ngOnInit(): void {
+
+  }
+  // title = 'msal-example';
+
+
+
+  login() {
+    this.authService.loginPopup()
+      .subscribe((results: AuthenticationResult)=>{
+        console.log(results);
+      });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { IPublicClientApplication, LogLevel, PublicClientApplication } from '@azure/msal-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+
+export function MSALInstanceFactory():IPublicClientApplication {
+  return new PublicClientApplication({
+    auth:{
+      clientId:'086166dd-f2bc-4f2d-acec-7a4925158d54',
+      redirectUri:'http://localhost:4200',
+      authority:'https://login.microsoftonline.com/common/'
+    }
+  })
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +24,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:MSAL_INSTANCE,
+      useFactory:MSALInstanceFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
